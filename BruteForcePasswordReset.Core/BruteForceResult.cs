1cbda0818f96bd788355399ae86dc7ccfe42e45a
aayushgautam1 <aayushgautam888@gmail.com>
2026-06-06 08:32:53 +0300

    added BruteForceResult class

---
+++ BruteForcePasswordReset.Core/BruteForceResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BruteForcePasswordReset.Core
+{
+    public class BruteForceResult
+    {
+        public bool Found { get; set; }
+        public string? Password { get; set; } = null;
+        public TimeSpan Elapsed { get; set; }
+        public long Attempts { get; set; }
+    }
+}
