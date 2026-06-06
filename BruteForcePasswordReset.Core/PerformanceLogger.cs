b4c1179983c9e0788782511ce97910108addc4eb
aayushgautam1 <aayushgautam888@gmail.com>
2026-06-06 08:38:47 +0300

    added performance logger

---
+++ BruteForcePasswordReset.Core/PerformanceLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BruteForcePasswordReset.Core
+{
+    public class PerformanceLogger
+    {
+        private readonly List<string> _entries = new List<string>();
+
+        public void Log(string message)
+        {
+            string entry = $"{DateTime.Now:HH:mm:ss} - {message}";
+            _entries.Add(entry);
+        }
+
+        public IEnumerable<string> GetEntries()
+        {
+            return _entries;
+        }
+
+        public void Clear()
+        {
+            _entries.Clear();
+        }
+    }
+}
