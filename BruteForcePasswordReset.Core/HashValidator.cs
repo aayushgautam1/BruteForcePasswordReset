29014a0a493d238259377c6e3a3dd2e260ab927f
aayushgautam1 <aayushgautam888@gmail.com>
2026-06-06 08:25:38 +0300

    added HashValidator class

---
+++ BruteForcePasswordReset.Core/HashValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace BruteForcePasswordReset.Core
+{
+    public class HashValidator
+    {
+        private readonly PasswordManager _pm = new PasswordManager();
+
+        // Returns true if candidate hashed equals targetHash
+        public bool IsMatch(string candidate, string targetHash)
+        {
+            var h = _pm.ComputeHash(candidate);
+            return string.Equals(h, targetHash, System.StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
